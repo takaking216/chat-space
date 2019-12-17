cab0babfbccb85510ba0a6b0d8f4e479a92a8f98
takaking216 <58413762+takaking216@users.noreply.github.com>
2019-12-17 12:05:05 +0900

    auto

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,7 @@
 $(function(){
   function buildHTML(message){
     if (message.image) {
-      var html = `<div class="message"> {message-id: #{message.id}}
+      var html = `<div class="message" data-message-id=${message.id}>
       <div class="upper-message">
         <div class="upper-message__user-name">
           ${message.user_name}
@@ -13,13 +13,13 @@ $(function(){
       <div class="lower-message">
         <p class="lower-message__content">
           ${message.content}
-        <img src"${message.image.url}">
+        <img src"${message.image}">
         </p>
       
       </div>
       </div>`
       }else{
-        var html = `<div class="message">
+        var html =  `<div class="message" data-message-id=${message.id}>
         <div class="upper-message">
           <div class="upper-message__user-name">
             ${message.user_name}
@@ -31,7 +31,6 @@ $(function(){
         <div class="lower-message">
           <p class="lower-message__content">
             ${message.content}
-          <img src"${message.image}">
           </p>
         
         </div>
@@ -39,6 +38,32 @@ $(function(){
       return html;
     }
   }
+
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    var groupList = window.location.href;
+    var result = groupList.match(/\/groups\/\d+\/messages/)
+
+  last_message_id = $('.message').last().data('message')
+  if(result){
+    $.ajax({
+      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      console.log('success');
+    })
+    .fail(function() {
+      console.log('error');
+      });
+    }
+  }
+
   $(`#new_message`).on('submit', function(e){
     e.preventDefault(e)
     var formData = new FormData(this);
@@ -63,4 +88,7 @@ $(function(){
       alert("メッセージ送信に失敗しました");
     })
   })
+  setInterval(reloadMessages, 7000);
 })
+
+
